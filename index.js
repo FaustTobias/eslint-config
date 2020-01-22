f331c3565600f3e9a1d93f16ff440b5e63bf0972
Tobias Faust <tf@ambimax.de>
2020-01-22 19:46:49 +0100

    Default to source type module

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
             extends: ["eslint:recommended", "plugin:prettier/recommended"],
             parserOptions: {
                 ecmaVersion: 11,
+                sourceType: "module",
             },
             env: {
                 browser: true,
